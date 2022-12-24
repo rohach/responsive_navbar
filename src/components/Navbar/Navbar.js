459b2344956a1506bfe557d08f24396dcb9ce3fa
rohanc01 <rohanchaulagain01@gmail.com>
2022-12-24 20:46:20 +0545

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="container Navbar">
+      <span className="nav__logo">DevLHB</span>
+      <div className={`nav__items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/service">Service</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div
+        className={`nav__toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
